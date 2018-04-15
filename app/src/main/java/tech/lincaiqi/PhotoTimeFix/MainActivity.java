f853d42c24bbe5d2253e2515f5fe9905ad22b992
Neuron <liziyuan123456@yeah.net>
2018-04-16 00:09:41 +0800

    release ture 1.0

--- app/src/main/java/tech/lincaiqi/PhotoTimeFix/MainActivity.java
+++ app/src/main/java/tech/lincaiqi/PhotoTimeFix/MainActivity.java
@@ -119,11 +119,11 @@ public class MainActivity extends Activity {
                                 }
                             });
 
-                            try {
+                            /*try {
                                 Thread.sleep(500);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
-                            }
+                            }*/
 
                         }
 
